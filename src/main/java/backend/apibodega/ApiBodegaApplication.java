ec861afa258299149ab4c0ccff423c70b8f64ab3
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2024-11-02 13:43:24 -0500

    creacion de proyecto

---
+++ src/main/java/backend/apibodega/ApiBodegaApplication.java
@@ -0,0 +1,13 @@
+package backend.apibodega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBodegaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBodegaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/apibodega/ApiBodegaApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.apibodega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBodegaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
